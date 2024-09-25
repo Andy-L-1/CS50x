23f268d284c7f9d551d706fd1cbdbbec1ac019a2
Andy-L-1 <limc8a@gmail.com>
2024-09-25 19:30:09 +0800

    Updating pyramid recursion

--- Day 3/pyramid.py
+++ Day 3/pyramid.py
@@ -11,13 +11,28 @@ def normal_pyramid(rows):
 def recursive_pyramid(rows):
     if rows == 0:
         return 0
-    recursive_pyramid(rows - 1)
-
+    
     i = 0
     while i < rows:
         print("#", end = "")
         i += 1
     print("")
 
-# recursive_pyramid(5)
-normal_pyramid(6)
+    # Recursive property. If you want to reverse the pyramid, just put this at the start
+    recursive_pyramid(rows - 1)
+
+
+# My own experimentation with implementing the recursive pyramid
+def recursive_pyramid_2(rows):
+    if rows == 1:
+        print("#", end = "")
+    else:
+        i = 0
+        while i < rows:
+            recursive_pyramid_2(rows - 1)
+            # recursive_pyramid(1)
+            rows -= 1
+        print("")
+
+recursive_pyramid(4)
+recursive_pyramid_2(3)
